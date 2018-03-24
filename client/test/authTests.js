814fced9bc89432fa8f0ab6d99e295aade9f7517
DhruvManchala <dmanchala@gmail.com>
2018-03-24 13:40:11 +0530

    testing mocha with CircleCI. This should fail.

--- client/test/authTests.js
+++ client/test/authTests.js
@@ -2,10 +2,13 @@
 /* eslint func-names: 0 */
 /* eslint prefer-arrow-callback: 0 */
 
+const assert = require('assert');
 const { describe, it } = require('mocha');
 
 describe('stub', function() {
   describe('stub', function() {
-    it('stub');
+    it('stub', function() {
+      assert.fail('testing failure on CircleCI');
+    });
   });
 });
