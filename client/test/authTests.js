674b45e93de42f64a6a7f43069ef1f6b6b5c5cdd
DhruvManchala <dmanchala@gmail.com>
2018-03-24 13:46:40 +0530

    CircleCI fails a job if a mocha test fails. Removing intentional test failure.

--- client/test/authTests.js
+++ client/test/authTests.js
@@ -2,13 +2,10 @@
 /* eslint func-names: 0 */
 /* eslint prefer-arrow-callback: 0 */
 
-const assert = require('assert');
 const { describe, it } = require('mocha');
 
 describe('stub', function() {
   describe('stub', function() {
-    it('stub', function() {
-      assert.fail('testing failure on CircleCI');
-    });
+    it('stub');
   });
 });
