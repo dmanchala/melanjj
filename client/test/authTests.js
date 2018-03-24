869ee435fdeaa27919fcd89b32822566e92542a7
DhruvManchala <dmanchala@gmail.com>
2018-03-24 13:19:39 +0530

    set up mocha and created dummy client and server test files

---
+++ client/test/authTests.js
@@ -0,0 +1,11 @@
+/* eslint space-before-function-paren: 0 */
+/* eslint func-names: 0 */
+/* eslint prefer-arrow-callback: 0 */
+
+const { describe, it } = require('mocha');
+
+describe('stub', function() {
+  describe('stub', function() {
+    it('stub');
+  });
+});
---
+++ test/authTests.js
@@ -0,0 +1,11 @@
+/* eslint space-before-function-paren: 0 */
+/* eslint func-names: 0 */
+/* eslint prefer-arrow-callback: 0 */
+
+const { describe, it } = require('mocha');
+
+describe('auth', function() {
+  describe('login', function() {
+    it('should block a non logged-in user from a protected resource');
+  });
+});
