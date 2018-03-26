42f9cd76144c52847779cc0bdb8b1bd766443fba
DhruvManchala <dmanchala@gmail.com>
2018-03-27 04:07:29 +0530

    refactored rendering of header menu options into separate component.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Menu, Layout } from 'antd';
 
 const { Item } = Menu;
 
-const Header = () => (
-  <Layout.Header className="header">
-    <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
-      <Item key="1" style={{ fontSize: 'xx-large' }}>
-        Melanjj
-      </Item>
-      <Item key="2">Million Song Dataset</Item>
-      <Item key="3">Log In</Item>
-      <Item key="4">Sign Up</Item>
-    </Menu>
-  </Layout.Header>
-);
+class Header extends Component {
+  /* eslint class-methods-use-this: 0 */
+  renderOptions() {
+    return [
+      <Item key="2">Million Song Dataset</Item>,
+      <Item key="3">Log In</Item>,
+      <Item key="4">Sign Up</Item>,
+    ];
+  }
+
+  render() {
+    return (
+      <Layout.Header className="header">
+        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+          <Item key="1" style={{ fontSize: 'xx-large' }}>
+            Melanjj
+          </Item>
+          {this.renderOptions()}
+        </Menu>
+      </Layout.Header>
+    );
+  }
+}
 
 export default Header;
