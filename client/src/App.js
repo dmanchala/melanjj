e88f8a7bdc76ad2d26d5e39e827ea2c57bf65bae
DhruvManchala <dmanchala@gmail.com>
2018-03-26 17:30:39 +0530

    created header component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,20 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Landing from './components/Landing';
 import DatasetDetail from './components/DatasetDetail';
-import SignupThankYOu from './components/SignupThankYou';
+import SignupThankYou from './components/SignupThankYou';
 
 const App = () => (
   <div>
     <BrowserRouter>
       <div>
-        <Route path="/" component={Landing} />
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route
+          exact
+          path="/datasets/melanjj/million-song-dataset"
+          component={DatasetDetail}
+        />
+        <Route exact path="/signup-thank-you" component={SignupThankYou} />
       </div>
     </BrowserRouter>
   </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Menu, Layout } from 'antd';
 
-const Header = () => <h2>Header</h2>;
+const { Item } = Menu;
+
+const Header = () => (
+  <Layout.Header className="header">
+    <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+      <Item key="1" style={{ fontSize: 'xx-large' }}>
+        Melanjj
+      </Item>
+      <Item key="2">Million Song Dataset</Item>
+      <Item key="3">Log In</Item>
+      <Item key="4">Sign Up</Item>
+    </Menu>
+  </Layout.Header>
+);
 
 export default Header;
