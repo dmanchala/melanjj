a06d6451c35324ff52b45649ac74f20d0968e098
DhruvManchala <dmanchala@gmail.com>
2018-03-26 21:08:04 +0530

    added styling to the root element of the body, and created the SignupThankYou component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
+import './App.css';
 import Header from './components/Header';
 import Landing from './components/Landing';
 import DatasetDetail from './components/DatasetDetail';
 import SignupThankYou from './components/SignupThankYou';
 
 const App = () => (
-  <div>
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route
-          exact
-          path="/datasets/melanjj/million-song-dataset"
-          component={DatasetDetail}
-        />
-        <Route exact path="/signup-thank-you" component={SignupThankYou} />
-      </div>
-    </BrowserRouter>
-  </div>
+  <BrowserRouter>
+    <Layout>
+      <Header />
+      <Route exact path="/" component={Landing} />
+      <Route
+        exact
+        path="/datasets/melanjj/million-song-dataset"
+        component={DatasetDetail}
+      />
+      <Route exact path="/signup-thank-you" component={SignupThankYou} />
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
--- client/src/components/SignupThankYou.js
+++ client/src/components/SignupThankYou.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Layout } from 'antd';
 
-const SignupThankYou = () => <h2>SignupThankYou</h2>;
+const { Content } = Layout;
+
+const SignupThankYou = () => (
+  <Content style={{ padding: 50 }}>
+    <div
+      style={{
+        background: '#fff',
+        padding: 24,
+        textAlign: 'center',
+      }}
+    >
+      <div style={{ fontSize: 64, fontWeight: 500 }}>
+        That&apos;s it. You did it.
+      </div>
+      <div style={{ fontSize: 'x-large' }}>
+        Thanks for signing up. We&apos;ll email you once your account is
+        activated.
+      </div>
+    </div>
+  </Content>
+);
 
 export default SignupThankYou;
