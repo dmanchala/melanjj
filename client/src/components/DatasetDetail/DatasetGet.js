90401357a7d758a63c82adddaae31486655dc862
DhruvManchala <dmanchala@gmail.com>
2018-03-27 03:32:38 +0530

    created a lot of the ui for DatasetQuery component

--- client/src/components/DatasetDetail/DatasetGet.js
+++ client/src/components/DatasetDetail/DatasetGet.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Button, Dropdown, Icon, Menu, Input, Col, Row } from 'antd';
 
-const DatasetQueryForm = () => (
-  <Card style={{ width: '50% ' }} bordered={false} />
+const { TextArea } = Input;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">1st menu item</Menu.Item>
+    <Menu.Item key="1">2nd menu item</Menu.Item>
+    <Menu.Item key="2">3rd menu item</Menu.Item>
+  </Menu>
 );
 
 const DatasetGet = () => (
-  <div style={{ padding: 25 }}>
-    <Card title="Get Dataset">
-      <DatasetQueryForm />
-    </Card>
+  <div style={{ padding: 10 }}>
+    <Row gutter={1}>
+      <Col span={8}>
+        <Card cover={<TextArea rows={20} />}>
+          <Button size="large" style={{ width: '100%', margin: '0 auto;' }}>
+            Run
+          </Button>
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Help">
+          <Card bordered={false}>How to use</Card>
+          <Card title="Query syntax" bordered={false}>
+            Query syntax
+          </Card>
+          <Card title="Example query" bordered={false}>
+            Example query
+          </Card>
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Collections">
+          <Card bordered={false}>
+            <Dropdown overlay={menu}>
+              <a className="ant-dropdown-link" href="#">
+                Collection Picker <Icon type="down" />
+              </a>
+            </Dropdown>
+          </Card>
+          <Card title="Summary" bordered={false}>
+            Summary
+          </Card>
+          <Card title="Columns" bordered={false}>
+            Columns
+          </Card>
+        </Card>
+      </Col>
+    </Row>
   </div>
 );
 
